b02a7a1922711219489c7cd6185bf1e8d2b58d77
Ryan Zheng <nogrethumphrey@gmail.com>
2020-05-22 11:57:04 +0200

    added springbootredis, springbootwebsocketrabbitmq, springbootwebsocket projects

--- springbootcassandra/src/test/java/com/qiusuo/springbootcassandra/SpringBootCassandraApplicationTests.java
+++ springbootcassandra/src/test/java/com/qiusuo/springbootcassandra/SpringBootCassandraApplicationTests.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @Transactional
-class SpringboottutorialApplicationTests {
+class SpringBootCassandraApplicationTests {
 
     @Autowired
     private BookService bookService;
--- springbootredis/src/main/java/com/qiusuo/springbootredis/domain/Student.java
+++ springbootredis/src/main/java/com/qiusuo/springbootredis/domain/Student.java
@@ -1,4 +1,24 @@
 package com.qiusuo.springbootredis.domain;
 
-public class Student {
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+
+/*
+In redis, each data structure is accessed by a key. The hash data structure is accessed by a key.
+In the data structure, we define the name of the key, and the value of the key.
+Since redis hash is about a dictionary. Then we could have many students.
+ */
+@RedisHash("Student")
+public class Student implements Serializable {
+
+    public enum Gender {
+        MALE, FEMALE
+    }
+
+    private String id;
+    private String name;
+    private Gender gender;
+    private int grade;
+    // ...
 }
