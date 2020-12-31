54c3ee4d7df1123f0408c2a411e50280be413632
Ryan Zheng <nogrethumphrey@gmail.com>
2020-12-31 18:27:54 +0100

    added R2DBCConfig

--- sprinbwebfluxtutorial/src/main/java/com/qiusuo/webfluxtutorial/config/R2DBCConfig.java
+++ sprinbwebfluxtutorial/src/main/java/com/qiusuo/webfluxtutorial/config/R2DBCConfig.java
@@ -1,12 +1,22 @@
 package com.qiusuo.webfluxtutorial.config;
 
+import io.r2dbc.h2.H2ConnectionConfiguration;
+import io.r2dbc.h2.H2ConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
-import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @Configuration
-public class R2DBCConfig {
+@EnableR2dbcRepositories
+public class R2DBCConfig  extends AbstractR2dbcConfiguration {
+
+    public ConnectionFactory connectionFactory() {
+        return new H2ConnectionFactory(
+                H2ConnectionConfiguration.builder()
+                        .url("mem:testdb;DB_CLOSE_DELAY=-1;")
+                        .username("sa")
+                        .build()
+        );
+    }
 }
--- sprinbwebfluxtutorial/src/main/java/com/qiusuo/webfluxtutorial/repository/BookRepository.java
+++ sprinbwebfluxtutorial/src/main/java/com/qiusuo/webfluxtutorial/repository/BookRepository.java
@@ -4,5 +4,4 @@ import com.qiusuo.webfluxtutorial.domain.Book;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 public interface BookRepository extends ReactiveCrudRepository<Book, String> {
-
 }
