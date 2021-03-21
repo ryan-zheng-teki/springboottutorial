f3b2c5baeaf3cb3c3a9be7e863ce66563e3743ca
Ryan.Zheng <ryan.zheng@unic.com>
2021-03-21 21:16:57 +0100

    added orderservice, orderaccountingservice,warehouseservice for kafkatutorial

---
+++ springkafkatutorial/warehouseservice/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    java
+    kotlin("jvm") version "1.4.21"
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
