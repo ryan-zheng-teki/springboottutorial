bcefd1834dadf182bc5744fcd6d48a04d049f9c3
Ryan.Zheng <ryan.zheng@unic.com>
2021-09-05 12:29:17 +0200

    update add thymeleaf dependency due to spring upgrade

--- springbootmessaging/build.gradle.kts
+++ springbootmessaging/build.gradle.kts
@@ -8,13 +8,15 @@ plugins {
 }
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-reactor-netty")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework:spring-messaging")
     implementation("org.springframework:spring-websocket")
     implementation("org.springframework.session:spring-session-data-redis")
-    implementation("org.springframework.data:spring-data-cassandra")
+    //implementation("org.springframework.data:spring-data-cassandra")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.11.1")
     implementation("com.google.guava:guava:11.0.2")
     implementation("org.springframework.boot:spring-boot-starter-security")
 }
+
